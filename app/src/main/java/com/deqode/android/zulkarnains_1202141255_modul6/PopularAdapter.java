d98946e91bfa202a7960a50841f5c7ecfb0a567f
zulkarnainzahlan <zahlanzulkarnain@gmail.com>
2018-04-01 23:41:11 +0700

    Initial commit

---
+++ app/src/main/java/com/deqode/android/zulkarnains_1202141255_modul6/PopularAdapter.java
@@ -0,0 +1,8 @@
+package com.deqode.android.zulkarnains_1202141255_modul6;
+
+/**
+ * Created by Naim on 4/1/2018.
+ */
+
+public class PopularAdapter {
+}
---
+++ app/src/main/java/com/deqode/android/zulkarnains_1202141255_modul6/PostPictActivity.java
@@ -0,0 +1,13 @@
+package com.deqode.android.zulkarnains_1202141255_modul6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostPictActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_pict);
+    }
+}
